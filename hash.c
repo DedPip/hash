7ecbcd76ab3abc4709e16fc99b422670b1b2104b
DedPip <125252491+DedPip@users.noreply.github.com>
2025-10-17 13:08:28 +0330

    Define MAX_TABLE_SIZE & Add hash func

--- hash.c
+++ hash.c
@@ -2,11 +2,25 @@
 
 #define MAX_NAME_SIZE 50
 #define MAX_INFO_SIZE 100
+#define MAX_TABLE_SIZE 10
 
 typedef struct {
     char name[MAX_NAME_SIZE];
     char info[MAX_INFO_SIZE];
 } Person;
+
+unsigned int hash(const char *str) {
+    if (str == NULL) {
+        printf("name is NULL\n");
+        return 1;
+    }
+    unsigned int sum = 0;
+    while (*str) {
+        sum += *str;
+        str++;
+    }
+    return sum % MAX_TABLE_SIZE;
+}
 
 int main(void) {
 
