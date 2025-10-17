8d21ab526488cab1dbce574faf870c8ad3d3648c
DedPip <125252491+DedPip@users.noreply.github.com>
2025-10-17 12:07:57 +0330

    Define MAX_SIZE for Person's members

--- hash.c
+++ hash.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+#define MAX_NAME_SIZE 50
+#define MAX_INFO_SIZE 100
+
 typedef struct {
-    char name;
-    char info;
+    char name[MAX_NAME_SIZE];
+    char info[MAX_INFO_SIZE];
 } Person;
 
 int main(void) {
