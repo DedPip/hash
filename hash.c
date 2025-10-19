dcd87a636ce49f17b800a03a71a4fa9ce127a62b
DedPip <125252491+DedPip@users.noreply.github.com>
2025-10-19 10:10:51 +0330

    Handle collisions with closed link functionality
    A pointer(*next) to a Person struct has been added
    to the Person struct, so that colliding Persons
    can be linked one after the other
    
    insert_person and search_person functions have
    been changed to insert and search in a closed link
    manner in case of collision.

--- hash.c
+++ hash.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAX_NAME_SIZE 50
 #define MAX_INFO_SIZE 100
 #define MAX_TABLE_SIZE 10
 #define DJB2_HASH_NUMBER 5381
 
-typedef struct {
+typedef struct Person {
     char name[MAX_NAME_SIZE];
     char info[MAX_INFO_SIZE];
+    struct Person *next;
 } Person;
 
-Person hashTable[MAX_TABLE_SIZE];
+Person *hashTable[MAX_TABLE_SIZE];
 
 unsigned int hash(const char *str);
 void insert_person(const char *name, const char *info);
 void search_person(const char *name);
 
 int main(void) {
-    
-    for (int i = 0; i < MAX_TABLE_SIZE; i++) {
-        memset(hashTable[i].name, 0, MAX_NAME_SIZE);
-        memset(hashTable[i].info, 0, MAX_INFO_SIZE);
-    }
 
     insert_person("Ali", "Age 20, City: Langarud");
     insert_person("Reza", "Age: 18 City: Lahijan");
@@ -55,7 +52,7 @@ int main(void) {
         }
 
         if (strcmp(name, "end") == 0) {
-            printf("Program stopped\n");
+            printf("\nProgram ended\n\n");
             break;
         }
 
@@ -81,21 +78,36 @@ unsigned int hash(const char *str) {
 
 void insert_person(const char *name, const char *info) {
     unsigned int hashIndex = hash(name);
-    if (hashTable[hashIndex].name[0] != '\0') {
-        printf("Insertion failed! Collision detected when inserting '%s' at index %u of %i it's occupied by '%s'\n", name, hashIndex, MAX_TABLE_SIZE, hashTable[hashIndex].name);
+    Person *newPerson = malloc(sizeof(Person));
+
+    if (!newPerson) {
+        printf("memory allocation has faild!\n");
         return;
     }
-    strcpy(hashTable[hashIndex].name, name);
-    strcpy(hashTable[hashIndex].info, info);
+
+    strcpy(newPerson->name, name);
+    strcpy(newPerson->info, info);
+    newPerson->next = NULL;
+
+    if (hashTable[hashIndex] == NULL) {
+        hashTable[hashIndex] = newPerson;
+    }
+    else {
+        newPerson->next = hashTable[hashIndex];
+        hashTable[hashIndex] = newPerson;
+    }
 }
 
 void search_person(const char *name) {
-    unsigned int hashIndex = hash(name);
-    if (strcmp(hashTable[hashIndex].name, name) == 0)
-    {
-        printf("Person found: %s, %s\n", name, hashTable[hashIndex].info);
+    unsigned int hashIndext = hash(name);
+    Person *current = hashTable[hashIndext];
+
+    while (current != NULL) {
+        if (strcmp(current->name, name) == 0) {
+            printf("Found person: name: %s info: %s\n", name, current->info);
+            return;
+        } 
+        current = current->next;
     }
-    else {
-        printf("Person not found\n");
-    }
+    printf("Person not found\n");
 }
